a567ba8fc6c85a9a12e600595b09875d987d8d07
James Butterworth <jjbsports16@hotmail.co.uk>
2021-10-27 17:43:38 +0100

    Added evaluate.py

---
+++ evaluate.py
@@ -0,0 +1,79 @@
+####################################################################
+# Code to evaluate network or genome performance on an environment #
+####################################################################
+
+
+def run(network, env_name, run_num, env_kwargs=None, render=False):
+
+    if env_kwargs is not None:
+        env = gym.make(env_name, **env_kwargs)
+    else:
+        env = gym.make(env_name)
+
+    env.seed(108)
+
+    reward = 0
+    done = False
+
+    if render:
+        env.render()
+
+    state = env.reset()
+    #Add domain parameters to input
+    if network.domain_params_input:
+        state = np.append(state, DOMAIN_PARAMETERS[run_num])
+
+    while not done:
+
+        if render:
+            env.render()
+
+        net_out = network.forward(state)
+
+        #Normalise output between action space bounds
+        action_vals = net_out * (env.action_space.high - env.action_space.low) + \
+                      env.action_space.low
+
+        state, r, done, info = env.step(action_vals)
+        if network.domain_params_input:
+            state = np.append(state, DOMAIN_PARAMETERS[run_num])
+
+        reward += r
+
+        '''
+        print("Net out: ", net_out)
+        print("Action vals: ", action_vals)
+        print("State: ", state)
+        print("Reward: ", r)
+        print("Total reward: ", reward)
+        '''
+
+    env.close()
+
+    return reward
+
+
+#Either pass in a genome and a network with the required architecture OR
+#a network with the weights already set
+def evaluate(genome=None, network=None,
+             env_name=None, env_kwargs=None, render=False,
+             verbosity=False, avg_fitnesses=False):
+
+    if genome is not None:
+        network.genotype = genome
+
+    rewards = []
+
+    #For a certain number of trials/env arguments
+    for run_num, kwargs in enumerate(env_kwargs):
+        r = run(network, env_name, run_num, kwargs, render)
+        rewards.append(r)
+
+        if verbosity:
+            print(kwargs)
+            print("Reward: ", r)
+
+    if avg_fitnesses:
+        return [sum(rewards) / len(rewards)]
+    else:
+        return rewards
--- main.py
+++ main.py
@@ -12,85 +12,10 @@ from domain_params import *
 from model_training import *
 from evo_utils import get_cmaes_centroid
 from constants import *
+from evaluate import *
 
 #Suppress scientific notation
 np.set_printoptions(suppress=True)
-
-
-def run(network, env_name, run_num, env_kwargs=None, render=False):
-
-    if env_kwargs is not None:
-        env = gym.make(env_name, **env_kwargs)
-    else:
-        env = gym.make(env_name)
-
-    env.seed(108)
-
-    reward = 0
-    done = False
-
-    if render:
-        env.render()
-
-    state = env.reset()
-    #Add domain parameters to input
-    if network.domain_params_input:
-        state = np.append(state, DOMAIN_PARAMETERS[run_num])
-
-    while not done:
-
-        if render:
-            env.render()
-
-        net_out = network.forward(state)
-
-        #Normalise output between action space bounds
-        action_vals = net_out * (env.action_space.high - env.action_space.low) + \
-                      env.action_space.low
-
-        state, r, done, info = env.step(action_vals)
-        if network.domain_params_input:
-            state = np.append(state, DOMAIN_PARAMETERS[run_num])
-
-        reward += r
-
-        '''
-        print("Net out: ", net_out)
-        print("Action vals: ", action_vals)
-        print("State: ", state)
-        print("Reward: ", r)
-        print("Total reward: ", reward)
-        '''
-
-    env.close()
-
-    return reward
-
-
-#Either pass in a genome and a network with the required architecture OR
-#a network with the weights already set
-def evaluate(genome=None, network=None,
-             env_name=None, env_kwargs=None, render=False,
-             verbosity=False, avg_fitnesses=False):
-
-    if genome is not None:
-        network.genotype = genome
-
-    rewards = []
-
-    #For a certain number of trials/env arguments
-    for run_num, kwargs in enumerate(env_kwargs):
-        r = run(network, env_name, run_num, kwargs, render)
-        rewards.append(r)
-
-        if verbosity:
-            print(kwargs)
-            print("Reward: ", r)
-
-    if avg_fitnesses:
-        return [sum(rewards) / len(rewards)]
-    else:
-        return rewards
 
 
 def evo_run(env_name, completion_fitness, dir_path, exp_dir_path):
