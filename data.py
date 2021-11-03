9a65c9ca023fa11caff24ca0f7d0b07a33783c12
James Butterworth <jjbsports16@hotmail.co.uk>
2021-11-03 14:47:38 +0000

    Cleaned data.py

--- data.py
+++ data.py
@@ -4,13 +4,14 @@ import csv
 from glob import glob
 import numpy as np
 
+
 def get_sub_folders(folders_dir,
                     dir_path='../IndirectEncodingsExperiments/lib/NeuroEvo/data/'):
 
     dir_path += folders_dir
 
     if os.path.isdir(dir_path):
-        #Get all folder names
+        # Get all folder names
         folder_names = [x[0] for x in os.walk(dir_path)][1:]
 
     else:
@@ -18,13 +19,14 @@ def get_sub_folders(folders_dir,
 
     return folder_names, dir_path
 
+
 def read_data(data_dir, dir_path='../IndirectEncodingsExperiments/lib/NeuroEvo/data/'):
 
-    #Get directories in data_dir
+    # Get directories in data_dir
     try:
         folder_paths, dir_path = get_sub_folders(data_dir, dir_path)
 
-        #If there are no directories in data_dir, then look for data in data_dir
+        # If there are no directories in data_dir, then look for data in data_dir
         if not folder_paths:
             folder_paths = [dir_path]
 
@@ -47,26 +49,26 @@ def read_data(data_dir, dir_path='../IndirectEncodingsExperiments/lib/NeuroEvo/d
 
                 for i, row in enumerate(csv_reader):
 
-                    #Convert to floats
+                    # Convert to floats
                     row = [float(i) for i in row]
 
-                    #First row is fitness and genotype
+                    # First row is fitness and genotype
                     if i == 0:
                         fitnesses.append(row[0])
                         genotypes.append(row[1:])
 
-                    #Second row is parameters, if they are there
+                    # Second row is parameters, if they are there
                     elif i == 1:
                         params.append(row)
 
-                    #If an IE was used the phenotype is on the third row
+                    # If an IE was used the phenotype is on the third row
                     elif i == 2:
                         phenotypes.append(row)
 
         except FileNotFoundError:
             sys.exit("Could not find file named: " + folder_paths[i])
 
-    #If phenotypes are not in file, make phenotypes equal to the genotypes
+    # If phenotypes are not in file, make phenotypes equal to the genotypes
     if not phenotypes:
         import copy
         phenotypes = copy.deepcopy(genotypes)
@@ -93,13 +95,12 @@ def dump_data(data, dir_path, file_name):
         f.close()
 
 
-#Calculates the name of the directory to store exp data in, it is looking for
-#exp_*next integer*
+# Calculates the name of the directory to store exp data in, it is looking for
+# exp_*next integer*
 def create_exp_dir_name(base_path):
 
     exp_full_dirs = glob(base_path + "/*")
 
-    #max_exp_num = None
     max_exp_num = 0
 
     if len(exp_full_dirs) == 0:
@@ -108,7 +109,7 @@ def create_exp_dir_name(base_path):
     else:
         for ed in exp_full_dirs:
 
-            #Get exp numbers in directory
+            # Get exp numbers in directory
             split_path = ed.split("/")
             exp_string = split_path[-1]
             try:
@@ -116,18 +117,19 @@ def create_exp_dir_name(base_path):
             except ValueError:
                 continue
 
-            #Find largest exp number
+            # Find largest exp number
             if max_exp_num is None:
                 max_exp_num = exp_num
             else:
                 if exp_num > max_exp_num:
                     max_exp_num = exp_num
 
-    return 'exp_' + str(max_exp_num+1)
+    return 'exp_' + str(max_exp_num + 1)
+
 
 def create_synthetic_data(code_size, num_data_points=500):
 
-    #return -10. + np.randn(500, code_size)
+    # return -10. + np.randn(500, code_size)
 
     means = np.zeros(500, 2)
     for i in range(means.size(0)):
@@ -138,25 +140,3 @@ def create_synthetic_data(code_size, num_data_points=500):
                 means[i][j] = -10.
 
     return means + np.randn(500, 2)
-
-
-###############################
-# Pandas data frame functions #
-###############################
-import pandas as pd
-
-#Computes data frame means and adds them to the data frame
-def compute_df_means(df):
-
-    #Add row means
-    df['mean'] = df.mean(axis=1)
-
-    #Add columns means
-    col_means = dict(list(zip(df.columns, df[:].mean())))
-    new_row = pd.Series(data=col_means, name='mean')
-    df = df.append(new_row, ignore_index=False)
-
-    return df
-
-
-
