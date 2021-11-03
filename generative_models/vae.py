b8b4e4aaf7917ff6061368c8ca64cfcfc34d2b3f
James Butterworth <jjbsports16@hotmail.co.uk>
2021-11-03 14:38:17 +0000

    VAE uses NeuralNetwork class

--- generative_models/vae.py
+++ generative_models/vae.py
@@ -1,48 +1,63 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from generative_models.batch_utils import generate_batches
-
-class Encoder(nn.Module):
-
-    def __init__(self, num_inputs, code_size):
-        super(Encoder, self).__init__()
-
-        self.l1 = nn.Linear(num_inputs, code_size)
-
-    def forward(self, x):
-        return self.l1(F.relu(x))
-
-class Decoder(nn.Module):
-
-    def __init__(self, code_size, num_outputs):
-        super(Decoder, self).__init__()
-
-        self.l1 = nn.Linear(code_size, num_outputs)
-
-    def forward(self, x):
-        return self.l1(x)
+from neural_network import NeuralNetwork
 
 
-class VAE(nn.Module):
+class VAE(torch.nn.Module):
 
-    def __init__(self, code_size, training_data):
+    def __init__(self, code_size, train_data_vec_size, num_hidden_layers,
+                 neurons_per_hidden_layer, read_decoder=False):
         super().__init__()
 
-        self.training_data = training_data
+        self._encoder = NeuralNetwork(
+            num_inputs=train_data_vec_size,
+            num_outputs=code_size,
+            num_hidden_layers=num_hidden_layers,
+            neurons_per_hidden_layer=neurons_per_hidden_layer,
+            final_activ_func=None)
 
-        self.encoder = Encoder(training_data.size(1), code_size)
-        self.hidden2mu = nn.Linear(code_size, code_size)
-        self.hidden2log_var = nn.Linear(code_size, code_size)
-        self.decoder = Decoder(code_size, training_data.size(1))
+        self._hidden2mu = torch.nn.Linear(code_size, code_size)
+        self._hidden2log_var = torch.nn.Linear(code_size, code_size)
 
-        self.optimiser = torch.optim.Adam(self.parameters(), lr=1e-3)
+        self._decoder = NeuralNetwork(
+            num_inputs=code_size,
+            num_outputs=train_data_vec_size,
+            num_hidden_layers=num_hidden_layers,
+            neurons_per_hidden_layer=neurons_per_hidden_layer,
+            final_activ_func=None)
+
+        self._optimiser = torch.optim.Adam(self.parameters(), lr=1e-3)
+
+    def train(self, train_data, num_epochs, batch_size):
+
+        batches = generate_batches(train_data, batch_size, shuffle=True)
+
+        for epoch in range(num_epochs):
+            loss = 0
+            for batch in batches:
+
+                self._optimiser.zero_grad()
+
+                mu, log_var = self._encode(batch)
+                hidden = self._reparametrise(mu, log_var)
+                outputs = self._decode(hidden)
+
+                train_loss = self._loss_function(mu, log_var,
+                                                 batch, outputs)
+                train_loss.backward()
+                self._optimiser.step()
+
+                loss += train_loss.item()
+
+            loss = loss / len(batches)
+
+            print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, num_epochs, loss))
 
     def forward(self, inputs, verbosity=False):
 
-        mu, log_var = self.encode(inputs)
-        hidden = self.reparametrise(mu, log_var)
-        output = self.decoder(hidden)
+        mu, log_var = self._encode(inputs)
+        hidden = self._reparametrise(mu, log_var)
+        output = self._decoder(hidden)
 
         if verbosity:
             for i in range(len(inputs)):
@@ -64,60 +79,34 @@ class VAE(nn.Module):
 
         return output
 
-    def encode(self, x):
+    def _encode(self, x):
 
-        hidden = self.encoder(x)
-        mu = self.hidden2mu(hidden)
-        log_var = self.hidden2log_var(hidden)
+        hidden = self._encoder(x)
+        mu = self._hidden2mu(hidden)
+        log_var = self._hidden2log_var(hidden)
         return mu, log_var
 
-    def decode(self, x):
+    def _decode(self, x):
+        return self._decoder(x)
 
-        return self.decoder(x)
-
-    def reparametrise(self, mu, log_var):
+    def _reparametrise(self, mu, log_var):
 
         sigma = torch.exp(0.5 * log_var)
-        z = torch.randn(size = (mu.size(0), mu.size(1)))
+        z = torch.randn(size=(mu.size(0), mu.size(1)))
         z = z.type_as(mu)
         return mu + sigma * z
 
-    def loss_function(self, mu, log_var, inputs, outputs):
+    def _loss_function(self, mu, log_var, inputs, outputs):
 
-        kl_loss = (-0.5*(1+log_var - mu**2 - torch.exp(log_var)).sum(dim=1)).mean(dim=0)
-        recon_loss_criterion = nn.MSELoss()
+        kl_loss = (-0.5 * (1 + log_var - mu**2 - torch.exp(log_var)).sum(dim=1)) \
+            .mean(dim=0)
+        recon_loss_criterion = torch.nn.MSELoss()
         reconstruction_loss = recon_loss_criterion(inputs, outputs)
 
         return reconstruction_loss + kl_loss
-
-    def train(self, num_epochs, batch_size):
-
-        batches = generate_batches(self.training_data, batch_size, shuffle=True)
-
-        for epoch in range(num_epochs):
-            loss = 0
-            for batch in batches:
-
-                self.optimiser.zero_grad()
-
-                mu, log_var = self.encode(batch)
-                hidden = self.reparametrise(mu, log_var)
-                outputs = self.decode(hidden)
-
-                train_loss = self.loss_function(mu, log_var,
-                                                batch, outputs)
-
-                train_loss.backward()
-                self.optimiser.step()
-
-                loss += train_loss.item()
-
-            loss = loss / len(batches)
-
-            print("epoch : {}/{}, loss = {:.6f}".format(epoch + 1, num_epochs, loss))
 
     def test(self):
         self(self.training_data, verbosity=True)
 
     def dump_decoder(self, file_path):
-        torch.save(self.decoder, file_path)
+        torch.save(self._decoder, file_path)
