e6619c6f19544e367e4eb0b049733ff6dbf91940
James Butterworth <jjbsports16@hotmail.co.uk>
2022-01-14 11:29:15 +0000

    New AE configs

--- config_generator.py
+++ config_generator.py
@@ -63,12 +63,13 @@ def dump_configs(configs):
 def main():
     base_config = read_configs(None)[0]
 
+    # Get centroid directories for universal controllers
     centroid_dirs = get_sub_folders('data/g6', final_sub_dirs_only=True,
                                     num_top_dirs_removed=1)
 
     hyper_params = [
         (["env", "domain_params"], [[0.0008], [0.0010], [0.0012], [0.0014], [0.0016]]),
-        (["optimiser", "cmaes", "centroid"], centroid_dirs)
+        (["ie", "decoder_file_num"], [0, 1, 2, 3, 4]),
     ]
 
     # Generate settings and then configs from those settings
