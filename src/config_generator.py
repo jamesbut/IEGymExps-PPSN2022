19565eb434924215bd94dd2568865d723c3caf83
James Butterworth <jjbsports16@hotmail.co.uk>
2022-03-15 15:48:46 +0000

    Added new models & new configs

--- src/config_generator.py
+++ src/config_generator.py
@@ -98,14 +98,14 @@ def main():
     base_config = read_configs(None)[0]
 
     # Get centroid directories for universal controllers
-    centroid_dirs = get_sub_folders('data/g3', final_sub_dirs_only=True,
-                                    num_top_dirs_removed=1)
+    # centroid_dirs = get_sub_folders('data/g3', final_sub_dirs_only=True,
+    #                                 num_top_dirs_removed=1)
 
     hyper_params = [
         (["env", "domain_params"], [[2.0], [3.0], [4.0], [5.0], [6.0]]),
         # (["env", "domain_params"], [[2.0, 4.0, 6.0]]),
-        (["optimiser", "cmaes", "centroid"], centroid_dirs)
-        # (["ie", "decoder_file_num"], [0, 1, 2, 3, 4])
+        # (["optimiser", "cmaes", "centroid"], centroid_dirs)
+        (["ie", "decoder_file_num"], [0, 1, 2, 3, 4])
     ]
 
     if len(hyper_params) > 2:
